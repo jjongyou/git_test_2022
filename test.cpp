5d138e72539bdf573895f5d3e57da35b1d14a0e7
jjong <youjong12@naver.com>
2022-01-04 22:01:50 +0900

    test

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+#define IP "192.168.1.13"
+#define packet 10
+using namespace std;
+
+int main() {
+	float data = packet;
+
+	cout << "data : " << data << endl;
+	string ip = IP;
+	cout << ip << endl;
+	return 0;
+}
