1b4208c47cd14442bd7e1c1ff6af1ccc44bdae6c
jjongyou <youjong12@naver.com>
2022-08-24 15:24:22 +0900

    fiexed test2

--- test2.cpp
+++ test2.cpp
@@ -4,6 +4,16 @@ using namespace std;
 int main() {
 	vector<int> v;
 //	v.push_back(1);
+	vector<int> v;
+	vector<int> v;
+	vector<int> v;
+	vector<int> v;
+	vector<int> v;
+//	v.push_back(1);
+//	v.push_back(1);
+//	v.push_back(1);
+//	v.push_back(1);
+//	v.push_back(1);
 	cout << "empty : " << v.empty() << endl;
 	cout << "empyu";
 	return 0;
