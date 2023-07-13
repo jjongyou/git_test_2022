ae4800c031401073aafb4bb9bcef109b01bc8bbe
jjongyou <youjong12@naver.com>
2023-07-13 15:18:38 +0900

    Work-in-progress
    test for SDN
    - change shadow table
    - change queue reassignment

--- test.cpp
+++ test.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <queue>
 
 #define IP "192.168.1.13"
 #define packet 10
 using namespace std;
+
+int add(int a, int b)
+{
+  return a + b;
+}
 
 int main() {
 	float data = packet;
@@ -11,5 +18,9 @@ int main() {
 	cout << "data : " << data << endl;
 	string ip = IP;
 	cout << ip << endl;
+        vector<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        cout << add(v[0], v[1]) << endl;
 	return 0;
 }
